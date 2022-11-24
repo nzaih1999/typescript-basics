c33cec93fce0ca0c7f3b58f29a82b4050034cc6f
nzai <nzaih18@gmail.com>
2022-11-24 09:11:22 +0300

    types

---
+++ Desktop/typescript/main.ts
@@ -0,0 +1,42 @@
+
+interface Car {
+    wheels:number; color:string
+}
+
+const car :Car = {wheels:4, color:'white'}
+
+interface Fruits {
+name:string;
+color:string;
+calories:string;
+[nutrients:string]:string
+}
+//extends additional properties
+
+
+const apple:Fruits = {name:'apple', color:'red', calories:'5'}
+
+apple.vitaminA="50mg"
+
+
+
+
+
+
+//enums and tuples
+
+enum Seasons {
+    winter, 
+    spring,
+    summer,
+    autumn
+}
+
+function seasonsGreetings (season:Seasons){
+ if(season===Seasons.autumn) return ("Hello")
+ if(season===Seasons.winter) return ("Hey")
+ if(season===Seasons.spring) return ("Hellllo")
+ if(season===Seasons.summer) return ("Summer is calling")
+}
+
+seasonsGreetings(Seasons.spring)
